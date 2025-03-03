c320c7b57a2a08043f5d626ff718d036760a9091
jonathan <cliffjonathan66@gmail.com>
2025-03-03 19:50:28 +0300

    h

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    let button = document.getElementById('button');
+
+    if (!button) {
+        console.warn("Creating button element for testing...");
+        button = document.createElement('button');
+        button.id = 'button';
+        document.body.appendChild(button);
+    }
+
+    button.addEventListener('click', function() {
+        console.log('Button was clicked!');
+    });
 }
+
